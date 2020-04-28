a1cd674b823d57bda4089afc280e8d1e130feb77
Bryam Castañeda <bryamrojas20@gmail.com>
2020-04-27 23:23:29 -0500

    Primer commit

---
+++ Procfile
@@ -0,0 +1,2 @@
+ web java -Dserver.port=$PORT $JAVA_OPTS -jar target/spring-boot-data-jpa-heroku-0.0.1-SNAPSHOT.jar 
+
---
+++ src/main/java/com/bolsadeideas/springboot/datajpa/app/controllers/ClienteRestController.java
@@ -0,0 +1,26 @@
+package com.bolsadeideas.springboot.datajpa.app.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bolsadeideas.springboot.datajpa.app.models.entity.Cliente;
+import com.bolsadeideas.springboot.datajpa.app.models.service.IClienteService;
+
+@RestController
+@RequestMapping("/api/clientes")
+public class ClienteRestController {
+	
+	@Autowired
+	private IClienteService clienteService;
+	
+	@GetMapping(value= "/listar")
+	public List<Cliente> listar() {
+		return clienteService.findAll();
+	}
+	
+
+}
